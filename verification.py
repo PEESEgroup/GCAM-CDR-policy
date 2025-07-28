50cc25725b952ee56010f36576d49ce010211bca
nathan-preuss <nnp25@cornell.edu>
2025-07-28 12:55:14 -0400

    testing verification pattern on ghg taxes

--- verification.py
+++ verification.py
@@ -1,10 +1,15 @@
+import pandas as pd
+
 import utilities
 
 
-def main(scenario_name):
+def main(scenario_name, config_fname):
     # get a list of files that need verification
     xml_files_to_build = utilities.build_from_scenario(scenario_name)
     files_to_verify = {}
+    dir = str(config_fname).replace("_", "/")
+    prefix = "./data/gcam_out/"
+    fpath = prefix+dir
 
     # TODO: may need to group certain types of files (i.e. CDR demand) together
     for file in xml_files_to_build:
@@ -14,6 +19,9 @@ def main(scenario_name):
 
     # for each file to verify, open the original .csv file and extract relevant data
     for key, value in files_to_verify:
+        ground_truth = utilities.open_csv(value)
+        # TODO: convert from dict to pd dataframe
+
         if key == "exo_CDR_demand":
             pass
         if key == "elastic_CDR_demand":
@@ -23,11 +31,16 @@ def main(scenario_name):
         if key == "ghg_constraint":
             pass
         if key == "ghg_tax":
-            pass
+            # TODO: query CO2 results
+            results = pd.read_csv(fpath+"CO2_prices.csv")
+            verify_ghg_tax(ground_truth, results)
         # TODO: add more file types
 
 
-    # TODO: for each file, run verification
+def verify_ghg_tax(ground_truth, results):
+    # TODO: compare ground_truth with the results and identify if any of the years have an error
+    years_with_error = []
+    return years_with_error
 
 
 if __name__ == '__main__':
