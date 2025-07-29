03c3496ce87416a36c1255cfab9917c3250d9e21
nathan-preuss <nnp25@cornell.edu>
2025-07-29 10:09:11 -0400

    remove unnecessary constants

--- constants.py
+++ constants.py
@@ -2,22 +2,12 @@ class GCAMConstants:
     """
     list of constants used in processing data from GCAM models
     """
-    # TODO: check to ensure that the list of versions and their corresponding file names are the ones you want to
-    #  process data for. The filename must be the same between the version and the GCAMDB_ filenames
-
-    version = [["test", "baseline"]]
-    GCAMDB_filenames = ["data/gcam_out/test/baseline/ref.csv"]
-
-    # TODO: ensure that this strings points to the correct location of the gcam/output/* database
-    #  directory names are of the form database_basexdb-<version-name>-<RCP>.
-    #  This location should only need to be set once
+    #  directory names are of the form database_basexdb-<version-name>-<baseline>.
     XML_DB_loc = "gcam/output/database_basexdb-"
     processed_map_loc = "data/maps/us_reprojected_shp.shp"
     basin_map_loc = "data/maps/reg_glu_boundaries_moirai_combined_3p1_0p5arcmin.shp"
 
     # other relevant constants
-    SSPs = ["SSP1", "SSP2", "SSP3", "SSP4", "SSP5"]
-    RCPs = ["6p0", "4p5", "3p7", "2p6", "1p9"]
     GCAM_region = ["USA", "Africa_Eastern", "Africa_Northern", "Africa_Southern", "Africa_Western", "Australia_NZ",
                    "Brazil", "Canada", "Central America and Caribbean", "Central Asia", "China", "EU-12", "EU-15",
                    "Europe_Eastern", "Europe_Non_EU", "European Free Trade Association", "India", "Indonesia", "Japan",
@@ -37,6 +27,3 @@ class GCAMConstants:
     x = [1990, 2005, 2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045, 2050, 2055, 2060, 2065, 2070, 2075, 2080, 2085,
          2090, 2095, 2100]
     plotting_x = [2025, 2030, 2035, 2040, 2045, 2050]
-
-    scenario_names = ["exoTest"]
-    baseline_names = ["default"]
