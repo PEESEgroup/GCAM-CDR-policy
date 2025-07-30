24d594e5b7d51b8b2be511f3adff5dc7409c5633
nathan-preuss <nnp25@cornell.edu>
2025-07-30 14:12:23 -0400

    pretty xmldb query list

--- produce_regional_queries.py
+++ produce_regional_queries.py
@@ -3,27 +3,40 @@ import xml.etree.cElementTree as ET
 from xml.dom import minidom
 
 new_file = ""
-with open("xml/to_regionalize.xml", "r") as f:
+with (open("xml/to_regionalize.xml", "r") as f):
     entry = f.read()
-    print(entry)
 
-    for i in c.GCAMConstants.GCAM_region.extend(c.GCAMConstants.USA_region).extend(c.GCAMConstants.grid_region):
+    # get list of regions
+    regions = []
+    regions.extend(c.GCAMConstants.GCAM_region)
+    regions.extend(c.GCAMConstants.USA_region)
+    regions.extend(c.GCAMConstants.grid_region)
+
+    # iterate through all regions
+    for i in regions:
         new_region = entry
         new_region = new_region.replace("Global", str(i))
         new_file = new_file + new_region
 
+    # remove instances of </queries><queries> and extra whitespace
+    new_file = new_file.replace("\n</queries>\n<queries>\n", "\n").replace("\n", ""). replace("  ", "")
+
     # append to query list
     root = ET.fromstring(new_file)
 
-    # read in the queries that are supposed to be global in
-    tree = ET.parse('/xml/global_queries.xml')
-    tree = tree.getroot()
-    childNodeList = root.findall(".//aQuery")
-    for node in childNodeList:
-        tree.append(node)
+    # read in the queries that are supposed to be global in scope
+    with (open("xml/global_queries.xml", "r") as global_queries):
+        queries = global_queries.read()
+        queries = queries.replace("\n", ""). replace("  ", "")
+        tree = ET.fromstring(queries)
 
-    # write out file
-    xmlstr = minidom.parseString(ET.tostring(tree, encoding="UTF-8", xml_declaration=True)).toprettyxml(
-        indent="   ")
-    with open("xml/query_list.xml", "w+") as f:
-        f.write(xmlstr)
+        childNodeList = root.findall(".//aQuery")
+        for node in childNodeList:
+            tree.append(node)
+
+        # write out file
+        xmlstr = minidom.parseString(ET.tostring(tree, encoding="UTF-8", xml_declaration=True)).toprettyxml(
+            indent="   ")
+
+        with open("xml/query_list.xml", "w+") as f:
+            f.write(xmlstr)
