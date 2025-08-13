7b683b918f0f19b62b4f28226c8baad1cf1cd50b
nathan-preuss <nnp25@cornell.edu>
2025-08-13 09:39:16 -0400

    get cdr price data

--- plotting_script.py
+++ plotting_script.py
@@ -11,9 +11,24 @@ def main(config_fname, reference_year):
     Main method for scripts used to plot figures and information for the article
     :return: N/A
     """
+    policy_cost(config_fname)
+
+
+def policy_cost(config_fname):
+    # check if it is a baseline scenario
     baseline = config_fname.split("_")[0]
     scenario = config_fname.split("_")[1]
+    if scenario == baseline:
+        # TODO build and write out baseline scenario
+        supply = data_manipulation.get_sensitivity_data([config_fname], "CDR_by_tech")
+        price = data_manipulation.get_sensitivity_data([config_fname], "CDR_by_tech")
+    else:
+        # TODO build and write out scenario policy cost, and compare to default
+        pass
 
+
+def marimekko(df):
+    pass
 
 if __name__ == '__main__':
     main("default_test", "2050")
