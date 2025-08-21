67176960635da8225de3a4004dd783d457516098
nathan-preuss <nnp25@cornell.edu>
2025-08-21 15:00:53 -0400

    ndc test_subsidy scenario

--- run_GCAM_scenarios.py
+++ run_GCAM_scenarios.py
@@ -442,8 +442,8 @@ def default_config(config_name):
 if __name__ == '__main__':
     all_configs = constants.GCAMConstants.scenario_names
     baseline_scenarios = constants.GCAMConstants.baseline_names
-    current_configs = ["ndc", "testsubsidy"]  # use camelCase
-    current_baseline = ["ndc"]  # use camelCase
+    current_configs = ["paris", "testsubsidy"]  # use camelCase
+    current_baseline = ["paris"]  # use camelCase
 
     # the scenario and baseline name should match for any baseline scenario
 
