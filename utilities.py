e3f8c0ac8227a3b9eb3029c1c8a187ba52387305
nathan-preuss <nnp25@cornell.edu>
2025-08-21 09:18:01 -0400

    wrong elastic cdr file attached

--- utilities.py
+++ utilities.py
@@ -51,7 +51,7 @@ def build_from_scenario(scenario_name):
             build_xml_config.XMLConfig(
                 data_files={"ghg_CDR_market_link": "./building_xml/inputs/linked_ghg_CDR_base_verify.csv",
                             "exo_CDR_demand_verify": "./building_xml/inputs/EXO_CDR_demand_verify_3419.csv",
-                            "elastic_CDR_demand_verify": "./building_xml/inputs/Elastic_CDR_demand_verify_19.csv"},
+                            "elastic_CDR_demand_verify": "./building_xml/inputs/Elastic_CDR_demand_verify_34.csv"},
                 xml_build_type="CDR Policy",
                 output_fname="default_CDRDemand.xml"
             ),
