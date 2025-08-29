42563591be136c0e694eca8ad36dcd3ca5360d4d
nathan-preuss <nnp25@cornell.edu>
2025-08-29 11:19:57 -0400

    remove invalid tech+region subsidy combos

--- building_xml/code/build_subsidy.py
+++ building_xml/code/build_subsidy.py
@@ -59,32 +59,36 @@ def build_subsidies(file):
         region = ET.SubElement(world, "region", name=str(area))
 
         # add technologies the subsidies apply to
-        invalid_regions_and_tech = ["AKTEW"]
+        invalid_regions_and_tech = ["AKTEW", "NVOEW", "IDOEW", "MTOEW", "WYOEW", "UTOEW", "COOEW", "AZOEW", "NMOEW",
+                                    "NDOEW", "SDOEW", "NEOEW", "KSOEW", "OKOEW", "MNOEW", "IAOEW", "MOOEW", "AROEW",
+                                    "WIOEW", "MIOEW", "INOEW", "KYOEW", "TNOEW", "OHOEW", "WVOEW", "VTOEW"]
         seen = {}
         for r, year, sector, ss, tech in subsidy:
-            if r+tech not in invalid_regions_and_tech:
+            subname = "_" + tech + "_subsidy"
+            print(area+tech)
+            if area+tech not in invalid_regions_and_tech:
                 if r + sector + ss + tech not in seen:
                     supply_sector = ET.SubElement(region, "supplysector", name=sector)
                     subsector = ET.SubElement(supply_sector, "subsector", name=ss)
                     technology = ET.SubElement(subsector, "technology", name=tech)
                     period = ET.SubElement(technology, "period", year=str(year))
-                    ET.SubElement(period, "input-subsidy", name="_" + tech + "_subsidy")
+                    ET.SubElement(period, "input-subsidy", name=subname)
                     seen[r + sector + ss + tech] = 0
                     # policy portfolio standard
                     region_link = markets[area]
-                    policy_portfolio_standard = ET.SubElement(region, "policy-portfolio-standard", name="_" + tech + "_subsidy")
+                    policy_portfolio_standard = ET.SubElement(region, "policy-portfolio-standard", name=subname)
                     ET.SubElement(policy_portfolio_standard, "policyType").text = "subsidy"
                     ET.SubElement(policy_portfolio_standard, "market").text = region_link["market"]
                 else:
                     # technology should be referenced before
                     period = ET.SubElement(technology, "period", year=str(year))
-                    ET.SubElement(period, "input-subsidy", name="_" + tech + "_subsidy")
+                    ET.SubElement(period, "input-subsidy", name=subname)
 
-                if str(year) not in seen:  # if the regions match
+                if str(year)+subname not in seen:  # if the regions match
                     year_sub = subsidy[r, year, sector, ss, tech]
                     # convert from $2025 USD/t CO2-eq to $1975/kg C
                     ET.SubElement(policy_portfolio_standard, "fixedTax", year=str(year)).text = str(
                         year_sub["fixedTax"] * constants.GCAMConstants.USD2025_tCO2_to_1975_kgC)
-                    seen[str(year)] = 0
+                    seen[str(year)+subname] = 0
 
     return scenario
