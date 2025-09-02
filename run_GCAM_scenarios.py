d259e7382fc50cf29dac468a891800c499ea4aaf
nathan-preuss <nnp25@cornell.edu>
2025-09-02 10:37:53 -0400

    s1 high policy files

--- run_GCAM_scenarios.py
+++ run_GCAM_scenarios.py
@@ -449,8 +449,7 @@ def default_config(config_name):
 if __name__ == '__main__':
     all_configs = constants.GCAMConstants.scenario_names
     baseline_scenarios = constants.GCAMConstants.baseline_names
-    current_configs = ["s2_high", "s2_low", "s2_nothing"]  # use camelCase
-    # "low_low" has an error in 2035
+    current_configs = ["s1h_high", "s1l_low", "s1n_nothing"]  # use camelCase
     # the scenario and baseline name should match for any baseline scenario
 
     # for debugging
