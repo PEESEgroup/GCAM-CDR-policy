e1e0ad1d634354f29ebb402a1f31a8d5a0a686e8
nathan-preuss <nnp25@cornell.edu>
2025-09-02 09:11:35 -0400

    avg price of CDR for procurement

---
+++ procurement.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+import constants
+
+
+def main(scenario):
+    baseline_df = pd.read_csv("data/data_analysis/supplementary_tables/"+scenario+"/"+scenario+"/sorted price and supply of CDR by technology.csv")
+
+    # calculate total cost
+    for j in constants.GCAMConstants.plotting_x:
+        baseline_df[str(j)+"total_cost"] = baseline_df[str(j)+"_price"] * baseline_df[str(j)+"_supply"]
+
+    # group and get total supply
+    baseline_df = baseline_df.groupby(["Units_price", "Units_supply"]).sum(min_count=1)
+
+    # calculate average price by year
+    for j in constants.GCAMConstants.plotting_x:
+        baseline_df[str(j)+"_avgCost"] = baseline_df[str(j)+"total_cost"] / baseline_df[str(j)+"_supply"]
+
+    # output calculations
+    baseline_df.to_csv("data/data_analysis/supplementary_tables/"+scenario+"/"+scenario+"/procurement_price.csv")
+
+
+if __name__ == '__main__':
+    for i in ["nothing", "low", "high"]:
+        main(i)
