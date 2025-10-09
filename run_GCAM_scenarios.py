c8d7ab9a6745bdb0ff341bef69aaa3308536c1e4
nathan-preuss <nnp25@cornell.edu>
2025-10-08 22:20:55 -0400

    results for no BECCS ITC

--- run_GCAM_scenarios.py
+++ run_GCAM_scenarios.py
@@ -447,8 +447,9 @@ def default_config(config_name):
 
 
 if __name__ == '__main__':
-    current_configs = ["s1-noBECCSitc-l_low", "s1-itc-h_high", "s1-itc-l_low"]  # use camelCase
+    current_configs = ["s1-itc-h_high", "s1-itc-l_low"]  # use camelCase
     # "s1l_low", "s1h_high", "s1-procure-h_high", "s1-procure-l_low",
+    # TODO: plotting curves for "s1-noBECCSitc-l_low",
     # the scenario and baseline name should match for any baseline scenario
 
     # for debugging
