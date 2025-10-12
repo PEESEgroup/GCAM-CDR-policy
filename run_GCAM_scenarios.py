90603aec7921633e6e46c7e26927d35d019da219
nathan-preuss <nnp25@cornell.edu>
2025-10-12 18:26:44 -0400

    high baseline update

--- run_GCAM_scenarios.py
+++ run_GCAM_scenarios.py
@@ -447,7 +447,7 @@ def default_config(config_name):
 
 
 if __name__ == '__main__':
-    current_configs = ["s1-itc-l_low"]  # use camelCase
+    current_configs = ["high_high"]  # use camelCase
     # "s1l_low", "s1h_high", "s1-procure-h_high", "s1-procure-l_low",
     # TODO: DAC subsidy payments in 2025 seem off
     # the scenario and baseline name should match for any baseline scenario
