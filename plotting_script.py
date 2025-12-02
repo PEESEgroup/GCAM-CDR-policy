861eb9653e2e3b5f3ac64d1c51453c881d09e7c2
nathan-preuss <nnp25@cornell.edu>
2025-12-02 08:35:49 -0500

    CDRIA finally has a working scenario

--- plotting_script.py
+++ plotting_script.py
@@ -20,11 +20,11 @@ def main(config_fname, reference_year):
     config_fname = config_fname.replace("_", "/")
     os.makedirs("./data/data_analysis/images/" + config_fname + "/", exist_ok=True)
     os.makedirs("data/data_analysis/supplementary_tables/" + config_fname + "/", exist_ok=True)
-    # CDR_cost(config_fname, reference_year)
-    # CDR_tech(config_fname, reference_year)
-    # social_cost(config_fname, reference_year)
-    # market_share(config_fname, reference_year)
-    # subsidy_expiration(config_fname, reference_year)
+    CDR_cost(config_fname, reference_year)
+    CDR_tech(config_fname, reference_year)
+    social_cost(config_fname, reference_year)
+    market_share(config_fname, reference_year)
+    subsidy_expiration(config_fname, reference_year)
     costs_and_benefits(config_fname, reference_year)
 
 
